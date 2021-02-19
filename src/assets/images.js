e1e6e67ecea063820a79afb55d46b0ef1bd570b1
julio flores <juliocesarflores12@gmail.com>
2021-02-19 12:23:45 -0400

    source solved

--- src/assets/images.js
+++ src/assets/images.js
@@ -2,9 +2,9 @@
 import menu from './png/menu.png';
 import back from './png/back.png';
 import profile from './png/profile.png';
+import commet from './png/note.png';
 const logo = 'https://www.geducar.com/sites/default/files/geducar-logo-footer.png';
 const welcome = 'https://cdni.iconscout.com/illustration/premium/thumb/welcome-2120254-1784891.png';
-const commet = 'https://cdn.icon-icons.com/icons2/1603/PNG/512/note-task-comment-message-edit-write_108613.png';
 const wait = 'https://cdni.iconscout.com/illustration/premium/thumb/robot-couple-waiting-for-job-interview-2741552-2294377.png';
 
 export {
@@ -14,5 +14,6 @@ export {
     commet,
     menu,
     back,
-    wait
+    wait,
+
 }
