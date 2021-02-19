32f972bb9a6680618b392cb15f4cb0125a0cce3b
julio flores <juliocesarflores12@gmail.com>
2021-02-19 10:23:52 -0400

    wait config

--- src/components/Wait.js
+++ src/components/Wait.js
@@ -4,6 +4,7 @@ import { wait } from '../assets/images';
 export const Wait = () => {
    return(
        <section className="wait-container">
+            <h2>Wait please...!</h2>
             <img src={`${wait}`}/>
        </section>
    )
