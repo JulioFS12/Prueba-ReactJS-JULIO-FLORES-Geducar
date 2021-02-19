97a96439ce63757ab7df9c4049e7bb0925e539c0
julio flores <juliocesarflores12@gmail.com>
2021-02-19 10:40:00 -0400

    solving img

--- src/assets/images.js
+++ src/assets/images.js
@@ -2,7 +2,7 @@
 
 const logo = 'https://www.geducar.com/sites/default/files/geducar-logo-footer.png';
 const welcome = 'https://cdni.iconscout.com/illustration/premium/thumb/welcome-2120254-1784891.png';
-const profile = 'https://cdn.icon-icons.com/icons2/2063/PNG/512/smartphone_smart_phone_mobile_profile_icon_124610.png'
+const profile = 'https://cdn.icon-icons.com/icons2/2063/PNG/512/smartphone_smart_phone_mobile_profile_icon_124610.png';
 const commet = 'https://cdn.icon-icons.com/icons2/1603/PNG/512/note-task-comment-message-edit-write_108613.png';
 const menu = 'https://icon-icons.com/icons2/906/PNG/48/bar-menu_icon-icons.com_70305.png';
 const back = 'https://cdn.icon-icons.com/icons2/259/PNG/128/ic_arrow_back_128_28226.png';
